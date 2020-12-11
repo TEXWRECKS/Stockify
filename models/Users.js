f98f60c4da850b5daecef9b2309a715e1754c7ad
Steve Rodriguez <smrodriguez88@gmail.com>
2020-12-10 19:56:25 -0600

    Added models for users and items

--- models/Users.js
+++ models/Users.js
@@ -3,18 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-    itemTitle: String,
-    itemUrl: String,
-    itemPrice: Number,
-    itemStatus: String,  //current status: in-stock or out-of-stock
-    itemAlert: String,  //alert type: price or in-stock
+    itemTitle: String,  // Name of item
+    itemUrl: String,  // URL to item
+    itemPrice: Number,  // latest item price
+    itemStatus: String,  // current status: in-stock or out-of-stock
+    itemAlert: String,  // alert type: price or in-stock
 })
 
 const userSchema = new Schema({
-    email: String,
-    firstName: String,
-    lastName: Number,
-    itemStatus: String,
+    email: String,  // User email
+    firstName: String,  // User first name
+    lastName: Number,  // User last name
+    userItems: [itemSchema],  // FK reference to items 
 })
 
 const User = mongoose.model("User", userSchema);
