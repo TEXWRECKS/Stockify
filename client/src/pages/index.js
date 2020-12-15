996047e411dc8fb16718a5290a1fd326b021f647
Emily Aguirre <emgal.aguirre@gmail.com>
2020-12-14 22:23:07 -0500

    added routes/ bootstrap not working

---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Index() {
+  return <div>This is the hompage</div>;
+}
+
+export default Index;
---
+++ client/src/pages/products.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function products() {
+  return <div>This is the product page</div>;
+}
+
+export default products;
