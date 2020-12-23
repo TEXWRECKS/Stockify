c03ccc2fe2370f3c0cc1368900f887546337693b
Greg Malone <atx76dragon@yahoo.com>
2020-12-22 22:49:21 -0600

    Worked on the onClick variable functions, adding routes for each possible button click, and adding prompts for verification of selections or lack thereof.  Added buttons to match, and questions/statements to match.  Commented out what each things added does, and what still needs to be done.

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -9,39 +9,113 @@ function Product(props) {
   // Creating a state that holds which question we are asking
   const[counter, setCounter] = useState(0);
   
+  
   // counter===0 question/button set event listener functions
-    // Question 1 "Yes" onclick function
+  // Question 1 "Yes" onclick function
   const question1Yes = (event) => {
     event.preventDefault()
+    // *** NEED to write code here to save the product to the saved products list/page
+
+    // changing counter to 1, to present the next question and set of yes and no buttons
+    setCounter(1)
   };
-    // Question 1 "No" onclick function
+
+  // Question 1 "No" onclick function
   const question1No = (event) => {
     event.preventDefault()
-  };
-  // end counter===0
+    // *** NEED to write code that will save the url that was searched, in case the user presses "Back" in the 404 option provided to them...
+    // ... if they click this "No" button
 
+    // setting the counter to 404 to prompt user to check their url entry and try again, or to return to the product page
+    setCounter(404)
+    };
+    // end counter===0
+
+  
   // counter===1 question/button set event listener functions
-    // Question 2 "Yes" onclick function
+  // Question 2 "Yes" onclick function
   const question2Yes = (event) => {
     event.preventDefault()
+    // *** NEED to write code here that will add this to the users list to be notified of
+
+    // changing counter to 2, to present the next question and set of yes and no buttons
+    setCounter(2)
   };
-    // Question 2 "No" onclick function
+  
+  // Question 2 "No" onclick function
   const question2No = (event) => {
     event.preventDefault()
+    // changing counter to 2, to present the next question and set of yes and no buttons
+    setCounter(2)
   };
   // end counter===1
 
+  
   // counter===2 question/button set event listener functions
-    // Question 3 "Yes" onclick function
+  // Question 3 "Yes" onclick function
   const question3Yes = (event) => {
     event.preventDefault()
+    // *** NEED to write code here that will add this to the users list to be notified of
+
+    setCounter(777)
   };
-    // Question 3 "No" onclick function
+  
+  // Question 3 "No" onclick function
   const question3No = (event) => {
     event.preventDefault()
+    // if statement to determine if question 2 answer was yes, then move to counter===777 (email form)
+    if (question2Yes = true) {
+      setCounter(777)
+    }
+    // or if question 2 was no, then move to counter 420 as the user did not select anything to be notified for
+    else if (question2No = true) {
+      setCounter(420)
+    }
   };
   // end counter===2
 
+
+  // counter===420 verification question button functions
+  // verificationQuestionYes (I'm Sure) on click function
+  const verificationQuestionYes = (event) => {
+    event.preventDefault()
+    // *** NEED to write code here that prompts the user to try another product...
+    // ... and that clears the search
+    setCounter(0)
+  }
+
+  // verificationQuestionNo (Take Me Back!) on click function
+  const verificationQuestionNo = (event) => {
+    event.preventDefault()
+    // *** Need to write code here that performs the url search again and brings it back to question two (first notification question)
+
+    setCounter(1)
+  }
+
+  
+  // counter===404 statement/button set event listener functions
+  const urlCheckClear = (event) => {
+    event.preventDefault()
+    // Resets the counter back to 0, to allow user to start a new search
+    setCounter(0)
+  };
+
+  const urlCheckReturn = (event) => {
+    event.preventDefault()
+    // *** NEED to write code that performs the search function with that saved url from question1No variable, IF this "Return" button is clicked...
+    // ... and sets the counter to (0) after performing that search.
+  };
+  // end counter===404 statement/button set
+
+  // counter===777
+  const emailSubmit = (event) => {
+    event.preventDefault()
+    // *** NEED to write code here that saves the users email (emailEntry) with their product & notification preferences
+    // *** NEED to write code here that uses the user's email (emailEntry) to send a notification to the user (first notification which confirms their product notification requests)
+    // *** NEED to write code here that triggers the web scrapping for the requested notification types for that product, and triggers notification on change
+  }
+
+  
   return (
     <div
     //   style={{
@@ -76,6 +150,29 @@ function Product(props) {
               Do you want to be notified of price changes?
             </Card.Subtitle>
             // Ends counter===2 question (Question 3)
+
+            // Counter===404 statement
+            // Message if the product search does not turn up the product the user is looking for
+            : counter===404 ?
+            <Card.Subtitle className="mb-2 test-muted product-subtext">
+              Please check your Amazon product URL and try again, or find another product to get notifications for
+            </Card.Subtitle>
+            // Ends counter===404 statement
+
+            // Counter===777 prompt
+            // Prompt for user to enter their email address and press submit to receive notifications requested
+            : counter===777 ?
+            <Card.Subtitle className="mb-2 test-muted product-subtext">
+              Enter your email address and press "Submit" to receive requested notifications
+            </Card.Subtitle>
+            // Ends counter===777 prompt
+
+            // Counter===420 question
+            : counter===420 ?
+            <Card.Subtitle className="mb-2 test-muted product-subtext">
+              Are you sure you don't want any availability or price change notifications for this product?
+            </Card.Subtitle>
+            // Ends counter===420 question
 
             : null}
 
@@ -134,6 +231,39 @@ function Product(props) {
                   </Button>
                   </>
                   // Ends counter===2 button set (for Question 3)
+
+                  // Counter===404 button set (for a "No" on Question 1)
+                  : counter===404 ?
+                  <>
+                  <Button onclick={() => urlCheckClear} variant="success" className="product-btn">
+                    Clear
+                  </Button>
+                  <Button onClick={() => urlCheckReturn} variant="success" className="product-btn">
+                    Return
+                  </Button>
+                  </>
+                  // Ends counter===404 button set
+
+                  // Counter===777 email address text entry and submit button
+                  : counter===777 ?
+                  <>
+                  <input handleFormSubmit={() => emailEntry} type="text"/> {/* *** NEED to add to this to provide text box for user to enter email address */}
+                  <Button onclick={() => emailSubmit} variant="success" className="product-btn">
+                    Submit
+                  </Button>
+                  </>
+                  // Ends counter 777
+
+                  // Counter===420 - verification question, confirming user is sure they don't want to be notified of product availability or price changes
+                  : counter===420 ?
+                  <>
+                  <Button onClick={() => verificationQuestionYes} variant="success" className="product-btn">
+                    I'm Sure
+                  </Button>
+                  <Button onClick={() => verificationQuestionNo} variant="success" className="product-btn">
+                    Take Me Back!
+                  </Button>
+                  </>
 
                   : null}
 
