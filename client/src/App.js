be4c28241b2394ecb19675dd38053cd4327f1bd7
Emily Aguirre <emgal.aguirre@gmail.com>
2021-01-08 13:58:38 -0500

    changed files to capital

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './components/Icons';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import Index from './pages/index';
-import Products from './pages/products';
+import Index from './pages/Index';
+import Products from './pages/Products';
 import Navbar from './components/Navbar';
 import ProductCard from './components/Product';
 import SavedProducts from './components/SavedProducts';
@@ -11,7 +11,7 @@ import Spinner from './components/Spinner';
 
 function App() {
   const [theme, setTheme] = useState('light');
-  const [isLoading, setIsLoading] = useState({})
+  const [isLoading, setIsLoading] = useState({});
   const [product, setProduct] = useState({
     itemTitle: '',
     itemUrl: '',
@@ -31,37 +31,50 @@ function App() {
     });
   }
 
-  function clearProductState(){
+  function clearProductState() {
     setProduct({
-      itemTitle: "",
-      itemUrl: "",
-      itemImage: "",
+      itemTitle: '',
+      itemUrl: '',
+      itemImage: '',
       itemPrice: 0,
-      itemStatus: "",
+      itemStatus: '',
       itemPriceAlert: 0,
-    })
+    });
   }
 
-  function readProductState(){
+  function readProductState() {
     return product;
   }
 
-  function updateIsLoadingState(bool){
-    setIsLoading(bool)
+  function updateIsLoadingState(bool) {
+    setIsLoading(bool);
   }
 
   return (
     <Router>
       <Navbar />
       <div>
-        <Route exact path="/" render={() => (<Index updateProductState={updateProductState} clearProductState={clearProductState} updateIsLoadingState={updateIsLoadingState}/>)} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Index
+              updateProductState={updateProductState}
+              clearProductState={clearProductState}
+              updateIsLoadingState={updateIsLoadingState}
+            />
+          )}
+        />
         <Route exact path="/products" component={Products} />
-        {isLoading == true &&
-        <Spinner />
-        }
-        {product.itemTitle != "" && 
-          <ProductCard item={product} updateProductState={updateProductState} clearProductState={clearProductState} readProductState={readProductState}/>
-        }
+        {isLoading == true && <Spinner />}
+        {product.itemTitle != '' && (
+          <ProductCard
+            item={product}
+            updateProductState={updateProductState}
+            clearProductState={clearProductState}
+            readProductState={readProductState}
+          />
+        )}
         <SavedProducts />
       </div>
     </Router>
